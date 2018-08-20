d783f0d3934648d52bf43a477830328a7f3c9057
Mirko Atanasov <Mirko.Atanasov@dh.com>
2018-08-20 14:34:31 -0400

    adding some logging statements to track the action, the original state and the new state in the product reducer

--- APM-Demo0/src/app/products/state/products-state-reducer.ts
+++ APM-Demo0/src/app/products/state/products-state-reducer.ts
@@ -1,11 +1,21 @@
 export function ProductsStateReducer(state, action) {
+
+    console.log(`action : `, JSON.stringify(action));
+    console.log(`original state : `, JSON.stringify(state));
+
     switch(action.type){
 
         case 'TOGGLE_SHOW_PRODUCT_CODE':
-            return {
+
+            let newState =  {
                 ...state, //'spread the existing state object as set of properties' - effectively copies the state and treats it as set of individual propertiesr
                 showProductCode : action.payload //if the showProductCode does not exist it will add it but if it's in the spread state it will update it
             }
+            
+            console.log(`new state : `, JSON.stringify(newState));
+
+            return newState;
+
         default:
             return state;
     }
