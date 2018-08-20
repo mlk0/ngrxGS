d4736985ffd84b568bc9ebc0b1faae8528c03982
Mirko Atanasov <Mirko.Atanasov@dh.com>
2018-08-20 14:28:39 -0400

    added case segment within the switch that will need to handle the action with type TOGGLE_SHOW_PRODUCT_CODE

--- APM-Demo0/src/app/products/state/products-state-reducer.ts
+++ APM-Demo0/src/app/products/state/products-state-reducer.ts
@@ -1,6 +1,11 @@
 export function ProductsStateReducer(state, action) {
     switch(action.type){
 
+        case 'TOGLE_SHOW_PRODUCT_CODE':
+            return {
+                ...state, //'spread the existing state object as set of properties' - effectively copies the state and treats it as set of individual propertiesr
+                showProductCode : action.payload //if the showProductCode does not exist it will add it but if it's in the spread state it will update it
+            }
         default:
             return state;
     }
