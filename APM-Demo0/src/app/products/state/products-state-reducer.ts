1b4aff7de9052dd2033e5705acb5fab25c4d3980
Mirko Atanasov <Mirko.Atanasov@dh.com>
2018-08-23 11:31:12 -0400

    adding the ProductState that will be used as products state slice in the global app state

--- APM-Demo0/src/app/products/state/products-state-reducer.ts
+++ APM-Demo0/src/app/products/state/products-state-reducer.ts
@@ -1,17 +1,25 @@
+import { Product } from "../product";
+
+export interface ProductState {
+    showProductCode: boolean,
+    selectedProduct: Product,
+    products: Product[]
+}
+
 export function ProductsStateReducer(state, action) {
 
     console.log(`action : `, JSON.stringify(action));
     console.log(`original state : `, JSON.stringify(state));
 
-    switch(action.type){
+    switch (action.type) {
 
         case 'TOGGLE_SHOW_PRODUCT_CODE':
 
-            let newState =  {
+            let newState = {
                 ...state, //'spread the existing state object as set of properties' - effectively copies the state and treats it as set of individual propertiesr
-                showProductCode : action.payload //if the showProductCode does not exist it will add it but if it's in the spread state it will update it
+                showProductCode: action.payload //if the showProductCode does not exist it will add it but if it's in the spread state it will update it
             }
-            
+
             console.log(`new state : `, JSON.stringify(newState));
 
             return newState;
