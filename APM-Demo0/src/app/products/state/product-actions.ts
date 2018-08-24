7c68cc8d868ddb7b91996b7ab5f1fb1467d203cf
Mirko Atanasov <Mirko.Atanasov@dh.com>
2018-08-24 15:58:14 -0400

    adding product load action types for complex opeartions

--- APM-Demo0/src/app/products/state/product-actions.ts
+++ APM-Demo0/src/app/products/state/product-actions.ts
@@ -1,5 +1,6 @@
 import { Action } from "@ngrx/store";
 import { Product } from "../product";
+import { Action } from "rxjs/internal/scheduler/Action";
 
 //enum of all action types for the product feature
 export enum ProductActionTypes {
@@ -7,13 +8,16 @@ export enum ProductActionTypes {
     SetCurrentProduct = '[Product] Set Current Product',
     SetCurrentProductId = '[Product] Set Current Product Id',
     ClearCurrentProduct = '[Product] Clear Current Product',
-    InitializeCurrentProduct = '[Product] Initialize Current Product'
+    InitializeCurrentProduct = '[Product] Initialize Current Product',
+    LoadProducts = '[Product] Load Products',
+    LoadProductsSuccess = '[Products] Load Products Success',
+    LoadProductsFailure = '[Products] Load Products Failure'
 }
 
 //action creator
-export class ToggleShowProductCode implements Action{
+export class ToggleShowProductCode implements Action {
     readonly type: string = ProductActionTypes.ToggleShowProductCode;
-    constructor(public payload : boolean){
+    constructor(public payload: boolean) {
     }
 
     // public payload : boolean;
@@ -25,33 +29,54 @@ export class ToggleShowProductCode implements Action{
 
 
 export class SetCurrentProduct implements Action {
-    readonly type : string = ProductActionTypes.SetCurrentProduct;
-    constructor(public payload : Product){}
+    readonly type: string = ProductActionTypes.SetCurrentProduct;
+    constructor(public payload: Product) { }
 }
 
-export class SetCurrentProductId implements Action{
+export class SetCurrentProductId implements Action {
     readonly type: string = ProductActionTypes.SetCurrentProductId;
-    constructor(public payload : number){
+    constructor(public payload: number) {
     }
 
 }
 
- 
+
 export class ClearCurrentProduct implements Action {
     readonly type: string = ProductActionTypes.ClearCurrentProduct;
-    payload : any = null;
-//    constructor(){}
+    payload: any = null;
+    //    constructor(){}
 }
 
 export class InitializeCurrentProduct implements Action {
-    readonly type : string = ProductActionTypes.InitializeCurrentProduct;
-    payload : any = null;
+    readonly type: string = ProductActionTypes.InitializeCurrentProduct;
+    payload: any = null;
+}
+
+
+export class LoadProducts implements Action {
+    readonly type: string = ProductActionTypes.LoadProducts
+    constructor(public payload = null) { }
+}
+
+
+export class LoadProductsSuccess implements Action {
+    readonly type: string = ProductActionTypes.LoadProductsSuccess
+    constructor(public payload: Product[]) { }
+}
+
+
+export class LoadProductsFailure implements Action {
+    readonly type: string = ProductActionTypes.LoadProductsFailure
+    constructor(public payload: string) { }
+
 }
 
 
 //union type
-export type ProductActions = 
-ToggleShowProductCode 
+export type ProductActions =
+    ToggleShowProductCode
     | SetCurrentProductId | SetCurrentProduct
     | ClearCurrentProduct
-    | InitializeCurrentProduct;
+    | InitializeCurrentProduct
+    | LoadProducts | LoadProductsSuccess | LoadProductsFailure
+    ;
